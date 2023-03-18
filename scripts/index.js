0cccaee26adda69f7cbe3c1b6d1dfeaf96b95603
John Ipson <john@devpipeline.com>
2023-03-18 17:17:55 -0600

    added styling and colors to counter

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,28 @@
 const incCount = () => {
   const count = document.getElementById("count");
-
   count.textContent = parseInt(count.textContent) + 1;
+  styleText(count);
 };
+
 const decCount = () => {
   const count = document.getElementById("count");
-
   count.textContent = parseInt(count.textContent) - 1;
+  styleText(count);
 };
 const resetCount = () => {
   const count = document.getElementById("count");
   count.textContent = 0;
+  styleText(count);
+};
+
+const styleText = (val) => {
+  if (val.textContent > 0) {
+    val.style.color = "#00FF00";
+  } else if (val.textContent < 0) {
+    val.style.color = "#FF0000";
+  } else {
+    val.style.color = "#000000";
+  }
 };
 
 const incBtn = document.getElementById("add");
